e6620ea94289c0e2ea777b62480b9553668af373
Niko Heiskanen <henipe@gmail.com>
2025-12-01 18:54:51 +0200

    cleanup

--- lib/days/day1.ex
+++ lib/days/day1.ex
@@ -42,13 +42,11 @@ defmodule Aoc2025.Day1 do
   defp move_dial(_dir, pos, 0, score), do: {pos, score}
 
   defp move_dial(dir, pos, count, score) do
-    pos =
-      case dir do
-        "L" -> Integer.mod(pos - 1, 100)
-        "R" -> Integer.mod(pos + 1, 100)
-      end
-
+    pos = move(dir, pos)
     score = if pos == 0, do: score + 1, else: score
     move_dial(dir, pos, count - 1, score)
   end
+
+  defp move("L", pos), do: Integer.mod(pos - 1, 100)
+  defp move("R", pos), do: Integer.mod(pos + 1, 100)
 end
